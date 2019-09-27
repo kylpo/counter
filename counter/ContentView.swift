2bad5eba8f2c2e5899528e7d6102fe797e85688d
kylpo <2kylepoole@gmail.com>
2019-09-27 09:00:28 -0400

    Initial Commit

---
+++ counter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  counter
+//
+//  Created by Kyle Poole on 9/27/19.
+//  Copyright © 2019 Kyle Poole. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
